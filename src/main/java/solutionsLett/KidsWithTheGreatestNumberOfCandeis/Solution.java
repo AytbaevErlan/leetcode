6ab4e1482d04588386462e397e3664e4ea6606b8
https://github.com/AytbaevErlan <irlanaytbaev@gmail.com>
2024-02-23 22:51:46 +0100

    leetcode 75

---
+++ src/main/java/solutionsLett/KidsWithTheGreatestNumberOfCandeis/Solution.java
@@ -0,0 +1,25 @@
+package solutionsLett.KidsWithTheGreatestNumberOfCandeis;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> boolList = new ArrayList<>();
+        int maxValue = Integer.MIN_VALUE;
+        for(int i = 0; i < candies.length; i++){
+
+            if(candies[i] > maxValue){
+                maxValue = candies[i];
+            }
+        }
+        for(int i = 0; i < candies.length; i++){
+            if (candies[i] + extraCandies >= maxValue){
+                boolList.add(true);
+            } else {
+                boolList.add(false);
+            }
+        }
+        return boolList;
+    }
+}
